6c491ca2e6f4f1e8ffdfd72f62453e6854b3508d
hanker <hanker756954@gmail.com>
2025-02-10 15:50:38 +0800

    分頁按鈕樣式

--- src/components/products/Pagination.js
+++ src/components/products/Pagination.js
@@ -2,13 +2,13 @@ export default function Pagination({ totalPages, currentPage, onPageChange }) {
   if (totalPages <= 1) return null; // 只有 1 頁時不顯示分頁按鈕
 
   return (
-    <div className="pagination d-flex justify-content-center mt-4">
+    <div className="pagination my-page-btn d-flex justify-content-center mt-4">
       {[...Array(totalPages)].map((_, index) => (
         <button
           key={index}
-          className={`btn ${
-            currentPage === index + 1 ? "btn-primary" : "btn-outline-primary"
-          } mx-1`}
+          className={`btn page-link ${
+            currentPage === index + 1 ? "my-active" : ""
+          }`}
           onClick={() => onPageChange(index + 1)}
         >
           {index + 1}
