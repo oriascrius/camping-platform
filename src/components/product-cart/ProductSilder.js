56ee40392a994b3e0bb05f3e1ec6f4de2207d160
hanker <hanker756954@gmail.com>
2025-03-06 15:36:09 +0800

    購物車熱門商品輪播圖RWD

--- src/components/product-cart/ProductSilder.js
+++ src/components/product-cart/ProductSilder.js
@@ -15,18 +15,17 @@ export default function ProductSlider() {
 
   useEffect(() => {
     const fetchHotProduct = async () => {
-      try{
+      try {
         const response = await fetch("/api/home/hot-product");
         const data = await response.json();
         setHotProduct(data);
-      }catch(error){
+      } catch (error) {
         console.error(error);
       }
     };
     fetchHotProduct();
-  },[]);
+  }, []);
 
-  
   return (
     <>
       <section className="d-flex product">
@@ -35,7 +34,13 @@ export default function ProductSlider() {
           <Swiper
             modules={[Autoplay, Pagination]} // 注册所需模块
             spaceBetween={73} // 每个滑块之间的距离
-            slidesPerView={4} // 一次显示一个滑块
+            slidesPerView={5} // 一次显示一个滑块
+            breakpoints={{
+              1024: { slidesPerView: 4, spaceBetween: 20 }, // 桌機
+              768: { slidesPerView: 3, spaceBetween: 15 }, // 平板
+              576: { slidesPerView: 2, spaceBetween: 10 }, // 手機 (576px ~ 768px)
+              0: { slidesPerView: 1, spaceBetween: 20 }, // **⚠️ 這一行確保 576px 以下維持 2 個**
+            }}
             centeredSlides={false} // 居中显示
             // loop={true} // 循环
             autoplay={{
@@ -45,114 +50,29 @@ export default function ProductSlider() {
             pagination={{ clickable: true }} // 启用分页
             className="mySwiper about-swiper"
           >
-          {hotProduct.length > 0 ? (
-            hotProduct.map((product) =>(
-              <SwiperSlide key={product.id}>
-              <Link href={`/products/${product.id}`}>
-                <span className="item">
-                  <div className="image">
-                    <img src={`/images/products/${product.main_image}`} alt="img" />
-                  </div>
-                  <h3 className="title">{product.name}</h3>
-                  <p className="price">
-                    $<span>{product.price}</span>
-                  </p>
-                </span>
-              </Link>
-            </SwiperSlide>
-            ))
-          ): (
-            <div>目前沒有熱門商品</div>
-          )}
-            {/* <SwiperSlide>
-              <a href="#">
-                <span className="item">
-                  <div className="image">
-                    <img src="/images/index/image 21.jpg" alt="img" />
-                  </div>
-                  <h3 className="title">3 人露營帳篷 MH100 Fresh</h3>
-                  <p className="price">
-                    $<span>2,999</span>
-                  </p>
-                </span>
-              </a>
-            </SwiperSlide> */}
+            {hotProduct.length > 0 ? (
+              hotProduct.map((product) => (
+                <SwiperSlide key={product.id}>
+                  <Link href={`/products/${product.id}`}>
+                    <span className="item">
+                      <div className="image">
+                        <img
+                          src={`/images/products/${product.main_image}`}
+                          alt="img"
+                        />
+                      </div>
+                      <h3 className="title">{product.name}</h3>
+                      <p className="price">
+                        $<span>{product.price}</span>
+                      </p>
+                    </span>
+                  </Link>
+                </SwiperSlide>
+              ))
+            ) : (
+              <div>目前沒有熱門商品</div>
+            )}
           </Swiper>
-          {/* <div className="product-slider">
-            <div className="product-item">
-              <a href="#" data-aos="fade-up">
-                <span className="item">
-                  <div className="image">
-            {/* <div className="swiper about-swiper">
-              <div className="swiper-wrapper">
-                <div className="swiper-slide">
-                  <a href="#" data-aos="fade-up">
-                    <span className="item">
-                      <div className="image">
-                        <img src="/images/index/image 21.jpg" alt="img" />
-                      </div>
-                      <h3 className="title">3 人露營帳篷 MH100 Fresh</h3>
-                      <p className="price">
-                        $<span>2,999</span>
-                      </p>
-                    </span>
-                  </a>
-                </div>
-                <div className="swiper-slide">
-                  <a href="#" data-aos="fade-up">
-                    <span className="item">
-                      <div className="image">
-                        <img src="/images/index/image 22.jpg" alt="img" />
-                      </div>
-                      <h3 className="title">3 人露營帳篷 MH100 Fresh</h3>
-                      <p className="price">
-                        $<span>2,999</span>
-                      </p>
-                    </span>
-                  </a>
-                </div>
-                <div className="swiper-slide">
-                  <a href="#" data-aos="fade-up">
-                    <span className="item">
-                      <div className="image">
-                        <img src="/images/index/image 63.jpg" alt />
-                      </div>
-                      <h3 className="title">4 人露營穿骨快開帳篷 Arpenaz</h3>
-                      <p className="price">
-                        $<span>1,499</span>
-                      </p>
-                    </span>
-                  </a>
-                </div>
-                <div className="swiper-slide">
-                  <a href="#" data-aos="fade-up">
-                    <span className="item">
-                      <div className="image">
-                        <img src="/images/index/image 65.jpg" alt />
-                      </div>
-                      <h3 className="title">3 人露營帳篷 MH100 Fresh</h3>
-                      <p className="price">
-                        $<span>2,999</span>
-                      </p>
-                    </span>
-                  </a>
-                </div>
-                <div className="swiper-slide">
-                  <a href="#" data-aos="fade-up">
-                    <span className="item">
-                      <div className="image">
-                        <img src="/images/index/image 65.jpg" alt />
-                      </div>
-                      <h3 className="title">3 人露營帳篷 MH100 Fresh</h3>
-                      <p className="price">
-                        $<span>2,999</span>
-                      </p>
-                    </span>
-                  </a>
-                </div>
-              </div>
-              <div className="swiper-pagination" />
-            </div> */}
         </article>
       </section>
     </>
